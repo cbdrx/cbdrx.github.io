cb4f5fe3c98ae74c0df644e7a523fcd63b1a0deb
cbdrx <boudreaux.christopher@gmail.com>
2016-06-23 17:05:38 -0500

    Abstracts the creation of the navbar to navbar.js and sets the active tab to avoid duplicate code

---
+++ navbar.js
@@ -0,0 +1,56 @@
+function createNavbar(){
+    var navbar = 
+    '<nav class="navbar-default navbar">' +
+        '<div class="container-fluid">' +
+            '<h1>Projects and Live Demos</h1><br>' +
+            '<ul class="nav nav-tabs nav-justified">' +
+                '<li id="AboutMeTab">' +
+                    '<a href="index.html"><span class="glyphicon glyphicon-user"></span>About Me</a>' +
+                '</li>'+
+                '<li id="ProjectsTab" class="disabled">'+
+                    '<a><span class="glyphicon glyphicon-pencil"></span>Projects Overview</a>'+
+                '</li>'+
+                '<li id="LiveDemoTab">' +
+                    '<a type="button" id="liveDemoDropdown" data-toggle="dropdown" aria-expande="false"><span class="glyphicon glyphicon-console"></span>Live Javascript Demos<span class="glyphicon glyphicon-triangle-bottom"></span></a>'+
+                    '<ul class="dropdown-menu">' +
+                        '<li><a href="cellular.html">Cellular Automata </a></li>'+
+                        '<li><a href="sorting.html">Sorting Algorithms </a></li>' +
+                    '</ul>'+
+                '</li>'+
+                '<li id="GithubTab">'+
+                    '<a href="https://github.com/cbdrx/"><span class="glyphicon glyphicon-equalizer" style="transform:rotate(90deg);"></span> Github </a>'+
+                '</li>'+
+            '</ul>'+
+        '</div>'+
+    '</nav>';
+    
+    return navbar;
+}
+
+function setActiveTab(page)
+{
+    switch(page)
+    {
+        case "AboutMe":
+        {
+            $('#AboutMeTab').addClass('active');
+            break;
+        }
+        case "Projects":
+        {
+            $('#ProjectsTab').addClass('active');
+            break;
+        }
+        case "LiveDemo":
+        {
+            $('#LiveDemoTab').addClass('active');
+            break;
+        }
+        default:
+        {
+            
+            break;
+        }
+            
+    }
+}
